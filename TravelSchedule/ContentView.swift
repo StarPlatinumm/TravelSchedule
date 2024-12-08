4e633b94f4ee18f05f1576a55101e48adac33fe8
Кривдин Артем <akrivdin@data-platform.ru>
2024-12-08 17:36:15 +0300

    NearestSettlementService

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -4,7 +4,7 @@ import OpenAPIURLSession
 
 struct ContentView: View {
     
-    func printStations() {
+    func printNearestStations() {
         let client = Client(
             serverURL: try! Servers.Server1.url(),
             transport: URLSessionTransport()
@@ -21,14 +21,35 @@ struct ContentView: View {
         }
     }
     
+    func printNearestSettlement() {
+        let client = Client(
+            serverURL: try! Servers.Server1.url(),
+            transport: URLSessionTransport()
+        )
+        
+        let service = NearestSettlementService(
+            client: client,
+            apikey: "4f5cb8fb-cdbd-4619-8ebf-aedd5c80cc35"
+        )
+        
+        Task {
+            let stations = try await service.getNearestSettlement(lat: 59.864177, lng: 30.319163, distance: 50)
+            print(stations)
+        }
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Print stations")
+            Text("Print nearest stations")
                 .onTapGesture {
-                    printStations()
+                    printNearestStations()
+                }
+            Text("Print nearest settlement")
+                .onTapGesture {
+                    printNearestSettlement()
                 }
         }
         .padding()
