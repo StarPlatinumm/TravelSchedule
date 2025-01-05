8355945501db9190061918da26e1db003d58b964
Кривдин Артем <akrivdin@data-platform.ru>
2025-01-05 17:32:11 +0300

    ErrorView

---
+++ TravelSchedule/Views/ErrorView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+enum ErrorType {
+    case noInternet
+    case serverError
+}
+
+struct ErrorView: View {
+    let errorType: ErrorType
+    let errorText: String
+    let errorImageName: String
+    
+    init(errorType: ErrorType) {
+        self.errorType = errorType
+        
+        switch errorType {
+        case .noInternet:
+            errorText = "Нет интернета"
+            errorImageName = "no-internet"
+        case .serverError:
+            errorText = "Ошибка сервера"
+            errorImageName = "server-error"
+        }
+    }
+    
+    var body: some View {
+        VStack (spacing: 16) {
+            Image(errorImageName)
+            Text(errorText)
+                .font(.system(size: 24, weight: .bold))
+        }
+    }
+}
+
+#Preview {
+    ErrorView(errorType: .noInternet)
+    ErrorView(errorType: .serverError)
+}
